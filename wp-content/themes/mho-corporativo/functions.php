23e041bdda7b0822c1d372e71119193cbaca98fc
Alvaro <alvaro.leonn@gmail.com>
2016-07-14 00:13:32 -0400

    Se integra el home y funciones principales

--- wp-content/themes/mho-corporativo/functions.php
+++ wp-content/themes/mho-corporativo/functions.php
@@ -8,7 +8,10 @@ add_theme_support('post-thumbnails');
 add_theme_support('menus');
 //show_admin_bar(false);
 
-add_image_size("location_list", 304, 223, true);
+add_image_size("equipo_big", 555, 390, true);
+add_image_size("equipo_small", 150, 150, true);
+add_image_size("quienes_somos_big", 494, 339, true);
+add_image_size("alianzas", 240, 220, true);
 
 /**
  * Eliminar logo de usuario
@@ -24,8 +27,172 @@ add_action('wp_ajax_nopriv_remove-logo', 'ajax_remove_logo');
 /**
  * Deja al final el snippet de Yoast
  */
-function jw_filter_yoast_seo_metabox() {
+function jw_filter_yoast_seo_metabox()
+{
     return 'low';
 }
 
-add_filter( 'wpseo_metabox_prio', 'jw_filter_yoast_seo_metabox', 10, 1 );
+add_filter('wpseo_metabox_prio', 'jw_filter_yoast_seo_metabox', 10, 1);
+
+/**
+ * Agrega a la tabla de Equipo
+ * @param array $taxonomies
+ * @return array
+ */
+function equipo_tax_columns($taxonomies)
+{
+    $taxonomies[] = 'pais';
+    return $taxonomies;
+}
+
+add_filter('manage_taxonomies_for_equipo_columns', 'equipo_tax_columns');
+
+
+/**
+ * Personalizar cabecera de columna (sin valor)
+ * @param array $columns
+ * @return array
+ */
+
+function edit_equipo_columns($columns)
+{
+
+    $columns['taxonomy-pais'] = 'País';
+    $columns['area'] = 'Área';
+    //return $columns;
+    return [
+        'cb' => $columns['cb'],
+        'title' => 'Nombre',
+        'cargo' => 'Cargo',
+        'area' => 'Área',
+        'taxonomy-pais' => 'País',
+        'date' => $columns['date'],
+    ];
+}
+
+add_filter('manage_edit-equipo_columns', 'edit_equipo_columns');
+
+/**
+ * Agrega valor a las columnas
+ * @param string $column
+ * @param int $post_id
+ * @return array
+ */
+function equipo_area_columns($column, $post_id)
+{
+
+    if ($column == 'area') {
+        $area = get_field('area', $post_id);
+
+        if (!$area)
+            echo 'Sin área';
+        else
+            echo $area->post_title;
+    }
+
+    if ($column == 'cargo') {
+        $cargo = get_field('cargo', $post_id);
+
+        if (!$cargo)
+            echo 'Sin cargo';
+        else
+            echo $cargo;
+    }
+}
+
+add_filter('manage_equipo_posts_custom_column', 'equipo_area_columns', 10, 2);
+
+/**
+ * Obtiene el equipo en orden de país y área
+ * @return array
+ */
+function get_equipo()
+{
+    $q = new WP_Query([
+        'post_type' => 'equipo',
+        'showposts' => -1,
+        'orderby' => 'menu_order',
+        'order' => 'asc'
+    ]);
+
+    $equipo_aux = [];
+
+    while ($q->have_posts()) {
+        $q->the_post();
+        /**
+         * @var WP_Term $pais
+         * @var WP_Post $area
+         */
+        $pais = get_the_terms($q->post, 'pais')[0];
+        $area = get_field('area');
+
+        $equipo_aux[$pais->slug]['pais'] = $pais->name;
+        $equipo_aux[$pais->slug]['data'][$area->post_name]['area'] = $area->post_title;
+        $equipo_aux[$pais->slug]['data'][$area->post_name]['area_url'] = get_permalink($area);
+        $equipo_aux[$pais->slug]['data'][$area->post_name]['area_color'] = get_field('color_principal', $area->ID);
+
+        $equipo_aux[$pais->slug]['data'][$area->post_name]['data'][] = [
+            'nombre' => $q->post->post_title,
+            'cargo' => get_field('cargo'),
+            'area' => $area->post_title,
+            'titulo' => get_field('titulo'),
+            'email' => get_field('email'),
+            'imagen' => get_field('imagen'),
+            'imagen_miniatura' => get_field('imagen_miniatura'),
+        ];
+    }
+    wp_reset_query();
+
+    /** @noinspection PhpInternalEntityUsedInspection */
+    $paises = get_terms([
+        'taxonomy' => 'pais'
+    ]);
+
+    /* Todo: Por mientras se ordena solo por países
+     * $areas = get_posts([
+        'post_type' => 'servicio',
+        'order' => 'menu_order',
+        'showposts' => -1
+    ]);*/
+
+    $equipo = [];
+
+    //Ordena por país
+    foreach ($paises as $pais) {
+        $equipo[$pais->slug] = $equipo_aux[$pais->slug];
+    }
+
+    return $equipo;
+}
+
+function get_alianzas()
+{
+    $q = new WP_Query([
+        'post_type' => 'alianza',
+        'showposts' => -1,
+        'orderby' => 'menu_order'
+    ]);
+
+    $i = 0;
+    $alianzas = [];
+    $j = 0;
+
+    while ($q->have_posts()) {
+        $q->the_post();
+
+        $alianzas[$j][] = [
+            'title' => get_the_title(),
+            'sitio_web' => get_field('sitio_web'),
+            'imagen' => get_the_post_thumbnail_url(null, 'alianzas'),
+        ];
+
+        if (($i + 1) % 3 == 0 && $i != 0) { // Se agrupa el array de 3 en 3
+            $j++;
+        }
+
+        $i++;
+    }
+    wp_reset_query();
+
+    return $alianzas;
+}
