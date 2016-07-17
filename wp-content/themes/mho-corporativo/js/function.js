000cc3c8ae2b27b20d048be1451f3e7965997cf2
cmonardesdev <teamultimedia@gmail.com>
2016-07-17 18:18:32 -0400

    Se agrega sección contacto con mapas de Perú y Chile.

--- wp-content/themes/mho-corporativo/js/function.js
+++ wp-content/themes/mho-corporativo/js/function.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $('.double-btn-container a.arrow-btn').on('click', function(e){
         e.preventDefault();
         var slider_id = $(this).attr('data-slider');
-        $('.team-area').hide();
+        $(this).parents('.equipo').hide();
         $("#"+slider_id).fadeIn();
         $(this).addClass('open');
     });
