814b81367fe82be9479e7a528e4367047f7cce94
cmonardesdev <teamultimedia@gmail.com>
2016-08-10 22:29:48 -0400

    Se agrega video al home y ajustes a validaciones de formularios

--- wp-content/themes/mho-corporativo/page-inicio.php
+++ wp-content/themes/mho-corporativo/page-inicio.php
@@ -102,6 +102,11 @@ echo ".servicios::after,.banner-clientes::before, .banner-clientes::after { back
                         <?php endif; ?>
                     </div>
                 </div>
+                <video src="http://mho.estudiomoca.com/wp-content/uploads/2016/07/detalle.mp4"
+                       width="100%" height="100%" autoplay="autoplay" controls loop muted>
+                    <code>Video</code>
+                    Tu navegador no soporta video HTML, por favor actualizar a las últimas versiones.
+                </video>
             </div>
         </article>
 
